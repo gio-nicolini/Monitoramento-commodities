c2ecf7e29e551827e26b5530142c4680c2ec23a1
Nicolini <nicolini.dev@gmail.com>
2024-08-31 19:59:47 -0300

    Inserindo variaveis de ambiente e testando

--- src/extract_load.py
+++ src/extract_load.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from pandas import DataFrame
 from yfinance import Ticker
 # pegar cotacao
-def get_data(ticker_name, periodo='1mo', intervalo='1d') -> DataFrame:
+def get_data(ticker_name, periodo='5d', intervalo='1d') -> DataFrame:
     
     ticker: Ticker = yf.Ticker(ticker_name)
     
@@ -21,13 +21,41 @@ def get_data(ticker_name, periodo='1mo', intervalo='1d') -> DataFrame:
 def get_all_data_commodities(comm_names: list[str]) -> DataFrame:
     return pd.concat([get_data(comm_name) for comm_name in comm_names])
 
+def save_db(df: DataFrame, schema: str):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+    
+    return
+
 if __name__ == "__main__":
     # Import variaveis de ambiente
+    
+    load_dotenv('.env')
+    
+    DB_HOST = os.getenv("DB_HOST_PROD")
+    DB_PORT = os.getenv("DB_PORT_PROD")
+    DB_NAME = os.getenv("DB_NAME_PROD")
+    DB_USER = os.getenv("DB_USER_PROD")
+    DB_PASS = os.getenv("DB_PASS_PROD")
+    DB_SCHEMA = os.getenv("DB_SCHEMA_PROD")
+    
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    
+    
+    print(DATABASE_URL)
+    engine = create_engine(DATABASE_URL)
+    print(engine)
+    
     commodities = ['CL=F', 'GC=F', 'SI=F']
+    
+    schema = 'public'
     
     data_comm: DataFrame = get_all_data_commodities(comm_names=commodities)
     
     print(data_comm)
+    
+    save_db(data_comm, schema)
+    
+    
         
 # concaternar ativos
 
